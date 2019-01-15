8ac843dc6a33b5b13ae423e64eed25e7b9fa96aa
Stefano Mandelli <linuxfree2@gmail.com>
2019-01-15 16:59:42 +0100

    Aggiustamenti nella rotine di integrazione 3D
    Ordinato e commantato la routine di integrazione 3D. Rimane
    il problema che e` solamente in 3D. Il programma e` scritto
    in modo tale da permettere una facile estensione per N-dim.
    
    La struttura Point con i suoi operatori, permette di calcolare
    il Jacobiano della trasformazione di normalizzazione degli
    estremi di integrazione in modo quasi ovvio.

--- corr_coef_julia/integrate_gauss.jl
+++ corr_coef_julia/integrate_gauss.jl
@@ -24,126 +24,136 @@ function gauss_3D(p::Point{Float64})
 end
 
 
-function Integrate(fun::Function, xl::Point{Float64}, xu::Point{Float64}, cal::Int64)
-	
-	#; Primo step, cal/3. valutazioni della funzione
+function Integrate(fun::Function, xl::Point{Float64}, xu::Point{Float64}, cal::Int64, dim::Float64)
+
+	# First Step: cal evaluations of the integrand using uniform distributed random number
 	rng = MersenneTwister(0)
-	pt = [Point{Float64}(rand(rng), rand(rng), rand(rng)) for n = 1:Int64(floor(calls/3.))]
+	pt = [Point{Float64}(rand(rng), rand(rng), rand(rng)) for n = 1:Int64(floor(dim * cal))]
 
-	#; Cambio coordinate, per mettermi nel range di integrazione scelto
+	# Coordinate transformation. ∫ₐᵇ f(x) dx  = ∫₀¹ f(y *(b-a) +a )*(b-a) dy
 	p = ( pt .* Ref(xu-xl) ) .+ Ref(xl)
-	density_unif = 1 / Int64(floor(calls/3.))
+	density_unif = 1 / Int64(floor(dim * calls))
 
-	#; Valuto l'integrale
+	# Integrand evaluations
 	f = [fun(point) * (xu.x-xl.x) * (xu.y-xl.y) * (xu.z-xl.z) * density_unif  for point in p ]
 	f_abs = abs.(f)
-	
+
+	# Perform the summation of intergrand and its absolute value
 	Integral      = sum(f)
-	Integral_abs  = sum(f_abs) 
+	Integral_abs  = sum(f_abs)
 
-	#; Valore dell'integrale di prova
-	Int_Vero = 3.33231
-	println("0. Integrale = ",(Integral-Int_Vero)," ", Integral)
-	
-	#; Calcolo la nuova densita` di probabilita`
+	# Check value - \Int_{-1}^{1} gauss_3D(x) dx
+	Int_True = 3.33231
+	#println("0. Integral = ",(Integral-Int_True)," ", Integral)
+
+	# Estimation of the new weight function
 	g = (f_abs ./ Integral_abs)
-	
-	#println("Controllo integrale pdf = ",sum(g))
-	print("Insert bin = " )
-	NSIDE = 14  #Int64(floor(1+log(2,cal/3.) ))# + log(2, 1+skewness(g)/((6*(cal/3. -2)/((cal/3. + 1)*(cal/3. + 2)))^0.5)  )) )
-	println(NSIDE)
 
-	#; posso fare qui la separazione dei punti nelle varie BOX
-	
-	w_m	 = zeros(Int64(NSIDE^3))
-	Int_box  = zeros(Int64(NSIDE^3))
-	Int_abs_box = zeros(Int64(NSIDE^3))
-	dix = diy = diz = 1.0/NSIDE
+	# println("Check PDF Integral = ",sum(g))
+	# I have to study hard to find a more suitable method to estimate the binning
+	NBIN = 15
+
+	# PDF approximation with stepper function
+	w_m	 = zeros(Int64(NBIN^3))
+	Int_box  = zeros(Int64(NBIN^3))
+	Int_abs_box = zeros(Int64(NBIN^3))
+	dix = diy = diz = 1.0/NBIN
+	Integral_avg = 0
 	for iter = 1:20
-		#; Organizzo la pdf nel BOX
+		# Evaluate the weigth on each BOX
 		for idx_g = 1:length(g)
 			box_id_x = Int64(ceil(pt[idx_g].x / dix)) -1
 			box_id_y = Int64(ceil(pt[idx_g].y / diy)) -1
 			box_id_z = Int64(ceil(pt[idx_g].z / diz)) -1
-			ID = (box_id_x) + (box_id_y)*NSIDE + (box_id_z)*NSIDE*NSIDE
-			w_m[ID+1] += g[idx_g]  
+			ID = (box_id_x) + (box_id_y)*NBIN + (box_id_z)*NBIN*NBIN
+			w_m[ID+1] += g[idx_g]
 		end
-	
+
+		# Check the weights distribution on each box and check if the
+		# PDF requirements is satisfied \Int_{\Omega} pdf(x) dx = 1
+
 		#println("Densita` in BOX")
 		#for i in 1:length(w_m)
 			#println(w_m[i])
 		#end
-		#println("Controllo Integrale PDF = ", sum(w_m))
-		
-		control=0
-		pt_2_all = Array{Point{Float64}}(undef, 0)
-		f_2_all  = Array{Float64,1}(undef, 0)
-		
-		for ix = (1:NSIDE)
-			for iy = (1:NSIDE)
-				for iz = (1:NSIDE)
+		#println("Check PDF Integral = ", sum(w_m))
+
+		# Evaluate the Integral using a non uniform random number distribution
+		total_hits = 0
+		pt_2_all   = Array{Point{Float64}}(undef, 0)
+		f_2_all    = Array{Float64,1}(undef, 0)
+
+		for ix = (1:NBIN)
+			for iy = (1:NBIN)
+				for iz = (1:NBIN)
 					p_start = Point{Float64}((ix-1) * dix , (iy-1) * diy, (iz-1) * diz)
 					p_final = Point{Float64}(ix * dix, iy * diy, iz * diz)
 
-					ID_BOX = ( (ix-1) + ( (iy-1) * NSIDE ) + ( (iz-1) * NSIDE * NSIDE ) ) + 1
-					
-					#; Densita` Uniforme
-					#density    = 1/(NSIDE*NSIDE*NSIDE)
+					ID_BOX = ( (ix-1) + ( (iy-1) * NBIN ) + ( (iz-1) * NBIN * NBIN ) ) + 1
 
-					#; Densita` Pesata
+					# Uniform density
+					#density    = 1/(NBIN*NBIN*NBIN)
+
+					# Non uniform weight PDF
 					density    = w_m[ID_BOX]
 
 
-					hit_in_box = Int64(floor(density * (cal/3.)))
-					control    += hit_in_box
+					hit_in_box = Int64(floor(density * (dim * cal)))
+					total_hits += hit_in_box
 					#println("Hit = ", hit_in_box)
 					if hit_in_box != 0
-						#; Qui va costruita la nuova densita` di punti
+
 						pt_2 = [Point{Float64}((p_final.x-p_start.x)*rand(rng)+p_start.x, (p_final.y-p_start.y)*rand(rng)+p_start.y,(p_final.z-p_start.z)*rand(rng)+p_start.z) for n = 1:hit_in_box]
-						#; Cambio coordinate
 						p_2 = ( pt_2 .* Ref(xu-xl) ) .+ Ref(xl)
-						
+						# Aggiustare quel /density!!!!!! Non e` conforme con quello sopra!!!!
 						f_2= [fun(point) * (xu.x-xl.x) * (xu.y-xl.y) * (xu.z-xl.z) / density  for point in p_2 ]
-							
+
 						append!(pt_2_all, pt_2)
 						append!(f_2_all, f_2)
 
 					else
 						continue
 					end
-					
+
 				end
 			end
 		end
-		
-		Integral += sum(f_2_all) / (control * NSIDE * NSIDE * NSIDE)
-		Integral_abs = sum(abs.(f_2_all))
-		g = (abs.(f_2_all) ./ Integral_abs) 
-		w_m     = zeros(Int64(NSIDE^3))
-	
-	
-		println(iter,". Integrale = ", (Integral / (iter+1)) - Int_Vero, " ", Integral / (iter + 1)  ) 	
-		
-	end
-		
 
-	
-	return Integral
+		Integral += sum(f_2_all) / (total_hits * NBIN * NBIN * NBIN)
+		Integral_abs = sum(abs.(f_2_all))
+		g = (abs.(f_2_all) ./ Integral_abs)
+		w_m     = zeros(Int64(NBIN^3))
+
+
+		println(iter,". Integral = ", (Integral / (iter+1)) - Int_True, " ", Integral / (iter + 1)  )
+		Integral_avg = Integral / (iter+1)
+
+
+		# check the point distribution in the cube
+		#plotly()
+		#x = [i.x for i in pt_2_all]
+		#y = [i.y for i in pt_2_all]
+		#z = [i.z for i in pt_2_all]
+		#scatter3d(x,y,z, markersize = 0.2, show = true, dpi=200, size=(1024,768))
+
+	end
+
+
+
+	return Integral_avg
 end
 
 
 #################### Integration #####################
 
 calls = 1000000
+dimensions = 3.
 
 xu = Point{Float64}(1., 1., 1.)
 xl = Point{Float64}(-1.,-1.,-1.)
 
 
 
-result = Integrate(gauss_3D, xl, xu, calls)
+result = Integrate(gauss_3D, xl, xu, calls, dimensions)
 
-
-
-
-
+println("Final Integral = ",result)
